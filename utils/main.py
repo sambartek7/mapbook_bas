758edec9dfc271d6f648922ab5aacfad70892a3b
sambartek7 <sambartek7@gmail.com>
2025-04-29 19:01:29 +0200

    add: map

--- utils/main.py
+++ utils/main.py
@@ -1,4 +1,4 @@
-from utils.controller import get_user_info, add_user, remove_user, update_user
+from utils.controller import get_user_info, add_user, remove_user, update_user, get_map
 from utils.model import users
 
 def main():
@@ -9,6 +9,7 @@ def main():
         print('2 - dodaj nowego uzytkownika')
         print('3 - usuń użytkownika')
         print('4 - edytuj użytkownika')
+        print('5 - przygotuj mape znajomych')
         print('=============MENU===============')
 
         choice = input('wybierz opcje menu: ')
@@ -18,6 +19,7 @@ def main():
         if choice == '2': add_user(users)
         if choice == '3': remove_user(users)
         if choice == '4': update_user(users)
+        if choice == '5': get_map(users)
 
 
 if __name__ == '__main__':
