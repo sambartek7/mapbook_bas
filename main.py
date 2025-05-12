1b166f84109f4fa59f641b47b2a3e2194a6f778c
sambartek7 <sambartek7@gmail.com>
2025-05-12 09:30:45 +0200

    wip : gui

--- main.py
+++ main.py
@@ -1,13 +1,91 @@
-users:list=[
-    {"name":"Mateusz","location":"Węgorzewo","posts": 100},
-    {"name": "Wiktoria", "location": "Chełm", "posts": 6},
-    {"name": "Sabina", "location": "Opole", "posts": 110},
-    {"name": "Weronika", "location": "Tomaszów Mazowiecki", "posts": 300},
-    {"name": "Julia", "location": "Zyrardów", "posts": 50},
+from tkinter import *
 
-]
 
-print(f"Witaj {users[0]["name"]}")
 
-for user in users:
-    print(f"Twój znajomy {user["name"]} z {user["location"]} opublikował {user["posts"]} postów")
+root = Tk()
+
+root.title('mapbook_bas')
+root.geometry('1200x700')
+
+ramka_lista_obiektow = Frame(root)
+ramka_formularz = Frame(root)
+ramka_szczegoly_obiektow = Frame(root)
+
+ramka_lista_obiektow.grid(row=0, column=0, padx=50)
+ramka_formularz.grid(row=0, column=1)
+ramka_szczegoly_obiektow.grid(row=1, column=0)
+
+#ramka_lista_obiektow
+
+label_lista_obiektow = Label(ramka_lista_obiektow, text='Lista znajomych')
+label_lista_obiektow.grid(row=0, column=0)
+
+listbox_lista_obiektów = Listbox(ramka_lista_obiektow)
+listbox_lista_obiektów.grid(row=1, column=0, columnspan=3)
+
+button_pokaz_szczegoly = Button(ramka_lista_obiektow, text='Pokaż szczegóły')
+button_pokaz_szczegoly.grid(row=3, column=0)
+
+button_usun_obiekt = Button(ramka_lista_obiektow, text='Usuń obiekt')
+button_usun_obiekt.grid(row=3, column=1)
+
+button_edytuj_obiekt = Button(ramka_lista_obiektow, text='Edytuj obiekt')
+button_edytuj_obiekt.grid(row=3, column=2)
+
+#ramka_formularz
+
+label_formularz = Label(ramka_formularz, text='Formularz')
+label_formularz.grid(row=0, column=1, columnspan=2)
+
+label_imie = Label(ramka_formularz, text='Imie')
+label_imie.grid(row=1, column=0, sticky=W)
+
+label_nazwisko = Label(ramka_formularz, text='Nazwisko')
+label_nazwisko.grid(row=2, column=0, sticky=W)
+
+label_miejscowosc = Label(ramka_formularz, text='Miejscowość')
+label_miejscowosc.grid(row=3, column=0, sticky=W)
+
+label_posty = Label(ramka_formularz, text='Liczba Postów')
+label_posty.grid(row=4, column=0, sticky=W)
+
+entry_imie = Entry(ramka_formularz)
+entry_imie.grid(row=1, column=1)
+
+entry_nazwisko = Entry(ramka_formularz)
+entry_nazwisko.grid(row=2, column=1)
+
+entry_miejscowosc = Entry(ramka_formularz)
+entry_miejscowosc.grid(row=3, column=1)
+
+entry_posty = Entry(ramka_formularz)
+entry_posty.grid(row=4, column=1)
+
+button_dodaj_obiekt = Button(ramka_formularz, text='Dodaj')
+button_dodaj_obiekt.grid(row=5, column=0, columnspan=2)
+
+#ramka_szczegoly_obiektu
+
+label_szczegoly_obiektu = Label(ramka_szczegoly_obiektow, text='Szczegóły użytkownika:')
+label_szczegoly_obiektu.grid(row=0, column=0)
+
+label_imie_szczegoly_obiektu = Label(ramka_szczegoly_obiektow, text='Imie:')
+label_imie_szczegoly_obiektu.grid(row=1, column=0)
+
+
+
+
+
+
+
+
+
+root.mainloop()
+
+
+
+
+
+
+
+
